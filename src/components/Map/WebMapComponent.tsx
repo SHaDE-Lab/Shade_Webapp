46c74e140f31395d066cffe46d8d1d5895f6c670
Niklas <ndoyle1313@gmail.com>
2022-11-02 15:48:40 -0700

    Changed basemap and progerss on ffeature query

--- src/components/Map/WebMapComponent.tsx
+++ src/components/Map/WebMapComponent.tsx
@@ -4,7 +4,7 @@ import WebMap from '@arcgis/core/WebMap'
 import Search from '@arcgis/core/widgets/Search'
 import Directions from '@arcgis/core/widgets/Directions'
 import RouteLayer from '@arcgis/core/layers/RouteLayer'
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
 
 import './index.css'
 
@@ -16,8 +16,13 @@ export default function WebMapComponent() {
     if (mapDiv.current) {
       const webmap = new WebMap({
         portalItem: {
-          id: 'aa1d3f80270146208328cf66d022e09c',
+          //Default basemap
+          //id: 'aa1d3f80270146208328cf66d022e09c',
+
+          //Plain Basemap
+          id: '00c8181753cd4673810a1ede1f52a922'
         },
+        
       })
 
       const view = new MapView({
@@ -36,23 +41,7 @@ export default function WebMapComponent() {
         view,
       })
 
-    /* Data Stored in Buildings
-        OBJECTID ( type: esriFieldTypeOID, alias: OBJECTID )
-        BLDG_NUMBER ( type: esriFieldTypeString, alias: BLDG_NUMBER, length: 5 )
-        BLDG_NAME ( type: esriFieldTypeString, alias: BLDG_NAME, length: 60 )
-        BLDG_CODE ( type: esriFieldTypeString, alias: BLDG_CODE, length: 10 )
-        BLDG_CITY ( type: esriFieldTypeString, alias: BLDG_CITY, length: 30 )
-        BLDG_STATE ( type: esriFieldTypeString, alias: BLDG_STATE, length: 2 )
-        BLDG_ZIP ( type: esriFieldTypeString, alias: BLDG_ZIP, length: 10 )
-        Description ( type: esriFieldTypeString, alias: Description, length: 2100 )
-        Type ( type: esriFieldTypeString, alias: Type, length: 16 )
-        map_name ( type: esriFieldTypeString, alias: map_name, length: 100 )
-        Image ( type: esriFieldTypeString, alias: Image, length: 2048 )
-        BLDG_ADDRESS ( type: esriFieldTypeString, alias: BLDG_ADDRESS, length: 50 )
-        Shape ( type: esriFieldTypeGeometry, alias: SHAPE )
-        Shape.STArea() ( type: esriFieldTypeDouble, alias: Shape.STArea() )
-        Shape.STLength() ( type: esriFieldTypeDouble, alias: Shape.STLength() )
-    */
+      //Adding Buildings Layer
       const buildingPopup = {
         "title": "{BLDG_NAME} ({BLDG_CODE})",
         "content": "<b>{BLDG_ADDRESS}</b><br><img src=\"{Image}\" alt=\"{Image of {BLDG_NAME}\"><br>{Description}<br><br><b>Building Number:</b> {BLDG_NUMBER}"
@@ -60,21 +49,41 @@ export default function WebMapComponent() {
     
       const buildings = new FeatureLayer({
         url: "https://gis.m.asu.edu/server/rest/services/Campus/CampusBuilding/MapServer",
+        //List of all Attributes contained in the building features
         outFields: ["OBJECTID","BLDG_NUMBER","BLDG_NAME","BLDG_CODE","BLDG_CITY","BLDG_STATE","BLDG_ZIP","Description"
         ,"Type","map_name","Image","BLDG_ADDRESS","Shape","Shape.STArea()","Shape.STLength()"],
         popupTemplate: buildingPopup
       })
       webmap.add(buildings)
 
+      //Query Buildings
+      view.on("click", function(event){
+        let query = buildings.createQuery();
+        query.geometry = view.toMap(event);  // the point location of the pointer
+        query.distance = 10;
+        query.units = "meters";
+        query.spatialRelationship = "intersects";  // this is the default
+        query.returnGeometry = true;
+        query.outFields = [ "BLDG_NAME" ];
+      
+        buildings.queryFeatures(query)
+          .then(function(response){
+            console.log("Query: ")
+            //As of right now, this only returns the number of features in that proximity
+            //Need to derive the information from the buildings
+            console.log(response.features.length)
+          });
+      });
+
       // Add the widget to the top-right corner of the view
       view.ui.add(searchWidget, 'top-right')
       view.ui.add(directions, 'top-right')
       
-
       // bonus - how many bookmarks in the webmap?
       webmap.when(() => {
         view.goTo({
-          center: [-111.9346, 33.419],
+          center: [-111.93, 33.419],
+          zoom: 15
         })
       })
     }
