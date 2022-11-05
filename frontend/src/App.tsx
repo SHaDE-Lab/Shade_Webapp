b69ef31e3a1ffb9d62e2122b26b3558a0a0bb866
Kit Donnelly <csdonnel@asu.edu>
2022-11-05 11:20:28 -0700

    Tweak Frontend For Concurrency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 import Header from './components/Header/Header'
 import InfoPage from './pages/InfoPage'
 import MapPage from './pages/MapPage'
 import NotFound from './pages/NotFound'
 
 export function App() {
+  // This is a test function that communica
+  const [data, setData] = useState()
+  const urlWithProxy = '/api/v1'
+
+  // This is a test function that works as an event handler,
+  // retrieving data from the backend using the test endpoint.
+  function getDataFromServer() {
+    axios
+      .get(urlWithProxy)
+      .then((res) => setData(res.data))
+      .catch((err) => {
+        console.log(err.response.data)
+      })
+  }
   return (
     <Routes>
       <Route path="/" element={<MapPage />} />
