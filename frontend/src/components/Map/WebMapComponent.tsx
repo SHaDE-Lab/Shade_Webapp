0de61a8877e1a83b172a3577472e43ca5576e30d
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2022-11-13 13:51:48 -0700

    Adjustments after merging in main

--- frontend/src/components/Map/WebMapComponent.tsx
+++ frontend/src/components/Map/WebMapComponent.tsx
@@ -6,30 +6,18 @@ import Directions from '@arcgis/core/widgets/Directions'
 import RouteLayer from '@arcgis/core/layers/RouteLayer'
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
 import LayerSearchSource from '@arcgis/core/widgets/Search/LayerSearchSource'
+import { useMap } from '../../context/MapContext'
 
 import './index.css'
 
 // TODO CREATE THIS AS A CONTEXT
 export default function WebMapComponent() {
   const mapDiv = useRef(null)
+  const view = useMap()
 
   useEffect(() => {
     if (mapDiv.current) {
-      const webmap = new WebMap({
-        portalItem: {
-          //Default basemap
-          //id: 'aa1d3f80270146208328cf66d022e09c',
-
-          //Plain Basemap
-          id: '00c8181753cd4673810a1ede1f52a922'
-        },
-        
-      })
-
-      const view = new MapView({
-        container: mapDiv.current,
-        map: webmap,
-      })
+      view.container = mapDiv.current
 
       const buildings = new FeatureLayer({
         url: "https://gis.m.asu.edu/server/rest/services/Campus/CampusBuilding/MapServer/0",
@@ -41,7 +29,9 @@ export default function WebMapComponent() {
           "content": "<b>{BLDG_ADDRESS}</b><br><img src=\"{Image}\" alt=\"{Image of {BLDG_NAME}\"><br>{Description}<br><br><b>Building Number:</b> {BLDG_NUMBER}"
         }
       })
-      webmap.add(buildings)
+
+      // if need access to base map use view.map
+      view.map.add(buildings)
 
       const searchSource = new LayerSearchSource({
         layer: buildings,
@@ -61,7 +51,8 @@ export default function WebMapComponent() {
         suggestionsEnabled: true,
         maxSuggestions: 5,
         minSuggestCharacters: 1,
-        sources: [searchSource]
+        sources: [searchSource],
+        id: 'searchWidget'
       })
 
       const routeLayer = new RouteLayer()
@@ -69,6 +60,7 @@ export default function WebMapComponent() {
       const directions = new Directions({
         layer: routeLayer,
         view,
+        id: 'directionsWidget'
       })
 
       //Query Buildings (For )
@@ -90,19 +82,18 @@ export default function WebMapComponent() {
           });
       });
 
-      // Add the widget to the top-right corner of the view
-      view.ui.add(searchWidget, 'top-right')
-      view.ui.add(directions, 'top-right')
-      
-      // bonus - how many bookmarks in the webmap?
-      webmap.when(() => {
+      // Add the widgets to the top-right corner of the view
+      if (!view.ui.find('searchWidget')) view.ui.add(searchWidget, 'top-right')
+      if (!view.ui.find('directionsWidget'))view.ui.add(directions, 'top-right')
+
+      view.when(() => {
         view.goTo({
           center: [-111.93, 33.419],
           zoom: 15
         })
       })
     }
-  }, [])
+  }, [view])
 
   return (
     <div
