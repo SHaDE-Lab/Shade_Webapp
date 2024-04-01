e2eb2aafcc881b6dc85cc36deeec66dedfd5e5a1
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2024-04-01 16:12:33 -0700

    add in unit converstion

--- src/components/Map/RouteCard.tsx
+++ src/components/Map/RouteCard.tsx
@@ -1,20 +1,44 @@
-import { useEffect, useState } from 'react'
-import { Card, CardContent, Typography } from '@mui/material'
+import { useState } from 'react'
+import { Switch, Card, CardContent, Typography, Box, FormControlLabel } from '@mui/material'
+import { convertToFahrenheit, convertToFeet } from '../../utils'
 
-const RouteCard = ( {average_mrt, length}) => {
+interface RouteCardProps {
+  average_mrt: number
+  length: number
+}
+
+const RouteCard = ( {average_mrt, length}: RouteCardProps) => {
+  const [useMetric, setMetric] = useState<boolean>(true)
+  
+  const displayTemp = (mrt: number)  =>{
+    return useMetric ? `${mrt.toFixed(2)}°C` : `${convertToFahrenheit(mrt).toFixed(2)}°F`
+  } 
+  const displayLength = (length: number) =>{
+    return useMetric ? `${length.toFixed(1)} m` : `${convertToFeet(length).toFixed(1)} ft`
+  }
 
   return (
     average_mrt != 0 && length != 0 && (
     <Card variant="outlined" style={{ borderRadius: 16 }}>
         <CardContent>
-          <Typography variant="h6" component="h2" gutterBottom>
-            Route Stats
+          <Box display="flex" justifyContent="space-evenly" gap={2}>
+            <Typography variant="h6" component="h2" gutterBottom>
+              Route Stats
+            </Typography>
+            <FormControlLabel
+              control = {<Switch
+                checked={useMetric}
+                onChange={() => setMetric(!useMetric)}
+              /> }
+              label={useMetric ? 'Metric' : 'Imperial'}
+              labelPlacement={'end'}
+            />
+          </Box>
+          <Typography variant="body1" component="p">
+            Average MRT: {displayTemp(average_mrt)}
           </Typography>
           <Typography variant="body1" component="p">
-            Average MRT: {average_mrt.toFixed(2)}°C
-          </Typography>
-          <Typography variant="body1" component="p">
-            Length: {length.toFixed(1)} meters
+            Length: {displayLength(length)}
           </Typography>
         </CardContent>
       </Card>
